7efe94cdda1420bc462270c7b297643da3b8824a
degui1 <gui.denez56@gmail.com>
2024-08-26 09:40:49 -0300

    ci: Add turborepo chace on deploy build

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   fonts,
   lineHeights,
   space,
-} from '@ignite-ui/tokens'
+} from '@degui-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
